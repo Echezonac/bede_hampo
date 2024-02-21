5f3a1262e816c941fd0a4222f64d532484137fe6
Bede E. Hampo <hampoherobede@gmail.com>
2024-02-21 22:35:29 +0100

    hero and about update1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,37 @@
 import { aboutBedeText } from "../../utils/about";
+import { useState } from 'react';
 import "./About.css";
 
 const About = () => {
+	const [showAll, setShowAll] = useState(false);
+
+	const toggleShowAll = () => {
+		setShowAll(!showAll);
+	};
 	return (
 		<div className='row py-5 about-page' id='about'>
 			{/* about content start */}
 			<div className='mx-auto col-xl-8 col-lg-6 col-md-12 col-sm-12 p-0'>
 				<div className='p-3'>
-					{aboutBedeText.map((str, index) => (
+					{aboutBedeText
+						.slice(0, showAll ? aboutBedeText.length : 1)
+						.map((str, index) => (
+							<p
+								key={index}
+								className='fs-6 p-2 text-justify mx-auto'
+							>
+								{str}
+							</p>
+						))}
+					{aboutBedeText.length > 1 && (
 						<p
-							key={index}
-							className='fs-6 p-3 text-justify mx-auto'
+						 style={{cursor:"pointer", textDecoration:"none"}}
+							onClick={toggleShowAll}
+							className='maroon-text p-0 ps-2'
 						>
-							{str}
+							{showAll ? "Show less >>" : "Show more >>"}
 						</p>
-					))}
+					)}
 				</div>
 			</div>
 			{/* about content end */}
